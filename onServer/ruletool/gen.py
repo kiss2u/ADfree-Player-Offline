ce34fbecb106e5f76ac7314456a607c574af232b
kafan15536900 <kafan15536900@gmail.com>
2014-11-29 00:59:22 +0800

    update mechanism
    add configlist to Server
    we can override user settings now

--- onServer/ruletool/gen.py
+++ onServer/ruletool/gen.py
@@ -1,6 +1,6 @@
 '''
 Created on 2014-8-9
-Modified on 2014-8-9
+Modified on 2014-11-28
 @author: Realz
 '''
 
@@ -30,15 +30,18 @@ def genrules(datalist):
 oredirectlist = open(path + 'oredirectlist.py','r')
 orefererslist = open(path + 'orefererslist.py','r')
 oproxylist = open(path + 'oproxylist.py','r')
+oconfiglist = open(path + 'oconfiglist.py','r')
 
 redirectlist = open(path + 'redirectlist','w')
 refererslist = open(path + 'refererslist','w')
 proxylist = open(path + 'proxylist','w')
+configlist = open(path + 'configlist','w')
 update = open(path + 'update','w')
 
 redirectlist.write(genrules(oredirectlist))
 refererslist.write(genrules(orefererslist))
 proxylist.write(genrules(oproxylist))
+configlist.write(genrules(oconfiglist))
 
 ttime = str(time.time()).split('.')
 timestamp = ttime[0] + ttime[1]
@@ -48,8 +51,10 @@ update.write(timestamp)
 oredirectlist.close()
 orefererslist.close()
 oproxylist.close()
+oconfiglist.close()
 
 redirectlist.close()
 refererslist.close()
 proxylist.close()
+configlist.close()
 update.close()
---
+++ onServer/ruletool/oconfiglist.py
@@ -0,0 +1,38 @@
+[
+    {
+        "name": "syoukuloader",
+        "status": "0"
+    },
+    {
+        "name": "syoukuplayer",
+        "status": "0"
+    },
+    {
+        "name": "sku6",
+        "status": "0"
+    },
+    {
+        "name": "studou",
+        "status": "0"
+    },
+    {
+        "name": "sletv",
+        "status": "0"
+    },
+    {
+        "name": "siqiyi",
+        "status": "0"
+    },
+    {
+        "name": "spps",
+        "status": "0"
+    },
+    {
+        "name": "ssohu",
+        "status": "0"
+    },
+    {
+        "name": "ssohu_live",
+        "status": "0"
+    }
+]
